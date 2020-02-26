3ae5ec777e9dca6b3a057c18695d02e41c7535cd
skeptical.knowledge <skeptical.knowledge@users.noreply.github.com>
2020-02-27 00:34:21 +0100

    Dodanie menu

--- katalog-samochodowy/katalog-samochodowy.cpp
+++ katalog-samochodowy/katalog-samochodowy.cpp
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <windows.h>
+#include <conio.h>
 
 class Pojazd
 {
@@ -19,7 +21,7 @@ public:
 			std::cout << rocznik << " ";
 			std::cout << pojemnosc << " ";
 			std::cout << przebieg << " ";
-			std::cout << typSkrzyniBiegow << " ";
+			std::cout << typSkrzyniBiegow << std::endl;
 		}
 };
 
@@ -27,15 +29,14 @@ std::vector <Pojazd> rejestr;
 
 void WypisanieListySamochodow()
 {
+	system("cls");
 	for (int i = 0; i < rejestr.size(); i++)
 	{
-		std::cout << rejestr[i].marka << " ";
-		std::cout << rejestr[i].model << " ";
-		std::cout << rejestr[i].pojemnosc << " ";
-		std::cout << rejestr[i].przebieg << " ";
-		std::cout << rejestr[i].rocznik << " ";
-		std::cout << rejestr[i].typSkrzyniBiegow << std::endl;
+		std::cout << i + 1 << " ";
+		rejestr[i].Wypisz();
+		//std::cout << rejestr[i].marka;
 	}
+	system("pause");
 }
 
 void WczytajZapisKataloguZPliku()
@@ -56,16 +57,37 @@ void WczytajZapisKataloguZPliku()
 			if(nowyPojazd.marka != "" && nowyPojazd.model != "" && nowyPojazd.pojemnosc != NULL && nowyPojazd.przebieg != NULL && nowyPojazd.rocznik != NULL)
 			rejestr.push_back(nowyPojazd);
 		}
+		std::cout << std::endl << "Wcztano!" << std::endl;
 	}
 	plik.close();
-
+	system("pause");
 }
 
 int main()
 {
-	
-	WczytajZapisKataloguZPliku();
-	WypisanieListySamochodow();
+	bool on = true;
+	do
+	{
+		system("CLS");
+		std::cout << "KATALOG SAMOCHODOWY" << std::endl << "WYBIERZ OPERACJE" <<std::endl;
+		std::cout << "1. Wczytaj baze danych z pliku" << std::endl;
+		std::cout << "2. Wypisz zawartosc rejestru" << std::endl;
+		std::cout << "3." << std::endl;
+		int numerOperacji = getchar();
+		switch (numerOperacji)
+		{
+		case '1':
+			WczytajZapisKataloguZPliku();
+			break;
+		case '2':
+			WypisanieListySamochodow();
+			break;
+		default:
+			break;
+		}
+		
+	} while (on);
+
 	return 0;
 }
 
